726445029427709144c9267c31f9aa2176c4de1f
diuguide <61917141+diuguide@users.noreply.github.com>
2020-07-18 23:02:08 -0400

    added write file sync creates goodreadme file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 
+
 // array of questions for user
 const questions = [
     {
@@ -68,9 +69,17 @@ const questions = [
     },
 
 ];
-
+//function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, generateMarkdown(data));
+};
 
 // function to initialize program
-inquirer.prompt(questions).then(function (answers) {
-    console.log(answers);
-});
+function init() {
+    inquirer.prompt(questions).then(function (answers) {
+        console.log(answers);
+
+        writeToFile("goodREADME.md", answers);
+    });
+};
+init();
