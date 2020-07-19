9fca0d8b1f68afa6cff42c1697c403c98dc10030
diuguide <61917141+diuguide@users.noreply.github.com>
2020-07-18 22:53:25 -0400

    first commit, package json files etc

---
+++ index.js
@@ -0,0 +1,76 @@
+// installations/dependecies
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter the name of your project?",
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "Please enter your name:",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a project description?",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please enter installation instructions:",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please enter usage instructions:",
+    },
+    {
+        type: "input",
+        name: "projectURL",
+        message: "Enter your webpage address.",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email:",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please select a license for your project.",
+        choices: [
+            "Apache License 2.0",
+            "GNU GPLv3",
+            "MIT",
+            "ISC",
+            "None"
+        ],
+    },
+    {
+        type: "list",
+        name: "contributors",
+        message: "Would you like other developers to contribute to your project?",
+        choices: [
+            "Yes",
+            "No"
+        ]
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please provide any relavent testing comments here:",
+    },
+
+];
+
+
+// function to initialize program
+inquirer.prompt(questions).then(function (answers) {
+    console.log(answers);
+});
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,28 @@
+function generateMarkdown(data) {
+  return `# ${data.title} 
+  ${data.license.badge} [![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](https://www.contributor-covenant.org/version/2/0/code_of_conduct/)
+  # Table of Contents
+  * [Project Description](#project-description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Tests](#tests)
+  * [License](#license)
+  * [Contributions](#contributions)
+  * [Questions](#questions)
+  # Project Description
+  ${data.description}
+  # Installation
+  ${data.installation}
+  # Usage
+  ${data.usage}
+  # Tests
+  ${data.tests}
+  # License
+  ${data.license.text}
+  # Contributions
+  ${data.contributors}
+  # Questions
+  For additional information, please e-mail the project manager [${data.name}](https://github.com/${data.github}/): ${data.email}.  
+`;
+}
+module.exports = generateMarkdown;
